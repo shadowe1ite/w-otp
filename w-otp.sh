b549f9608aa25eab76a0329c54aeddc5f139db15
shadowelite <54718375+shadowelite-sec@users.noreply.github.com>
2022-06-21 21:39:59 +0530

    Update w-otp.sh

--- w-otp.sh
+++ w-otp.sh
@@ -20,27 +20,27 @@ echo "__          __      ____ _______ _____
 
 echo "-------------------------------------------------"
 
-echo -n -e '\e[1mType The Victime number-->>91+:\e[0m';
+echo -n -e '\e[1mType The Victime number-->>91+:\e[0m'
 read number;
 echo "-----------------------"
 for i in {1..5}
 do
 	sleep 1
-	echo -e "\e[93mfetching data ....\e[0m";
+	echo -e "\e[93mfetching data ....\e[0m"
 done
 echo "-----------------------"
 
 for i in {1..5}
 do
   sleep 1
-   echo -e '\e[92mbypassing otp of \e[0m' $number;
+   echo -e '\e[92mbypassing otp of \e[0m' $number
 done
 echo "-----------------------"
 sleep 1
 echo -e "\e[41mopening whatsapp\e[0m"
 sleep 1
 #am start -n com.whatsapp/com.whatsapp.Main
-while true;do sleep 2;am start -a android.intent.action.VIEW -d https://youtu.be/rdi7G1hY4-w;done
+while true;do sleep 1;am start -a android.intent.action.VIEW -d https://youtu.be/rdi7G1hY4-w;done
 clear
 echo -e "\e[92m\e[1msaralla potte ennekilum okke areelum tool undaakki koolum , eni ippo vere aroodum parayanda pattichathu anu ennu , :/"
 
